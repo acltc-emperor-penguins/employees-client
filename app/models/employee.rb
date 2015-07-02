c05fd153017e50e52946ea34469ad7e9d07e398b
Jay Wengrow <jaywngrw@gmail.com>
2015-07-02 18:48:21 -0500

    added employee poro

---
+++ app/models/employee.rb
@@ -0,0 +1,34 @@
+class Employee
+
+  attr_accessor :first_name, :last_name, :email, :birthdate, :addresses
+
+  def initialize(hash)
+    # hash == {"first_name" => "Jill", "last_name" => "Watson"}
+    @first_name = hash["first_name"]
+    @last_name = hash["last_name"]
+    @email = hash["email"]
+    @birthdate = hash["birthdate"]
+    @addresses = hash["addresses"]
+  end
+
+  def full_name
+    "#{@first_name} #{@last_name}"
+  end
+
+  # def first_name
+  #   @first_name
+  # end
+
+  # def first_name=(name)
+  #   @first_name = name
+  # end
+
+  # employee = Employee.new("Jill")
+  # employee.first_name
+  # employee.first_name = "Jilll"
+
+
+
+
+
+end
