7c665997b66187452f92c142d87e5d4fd2b911ec
Jay Wengrow <jaywngrw@gmail.com>
2015-07-02 20:57:15 -0500

    allow user to delete employee

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/employees/new' => 'employees#new'
   get '/employees/:id' => 'employees#show'
   post '/employees' => 'employees#create'
-  
+  delete '/employees/:id' => 'employees#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
